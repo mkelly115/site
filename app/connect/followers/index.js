3dca4f7b54ab0b77ffc14167f7be241b58e9f93f
Michael <mikek222@gmail.com>
2025-04-28 15:58:03 -0400

    added server and client components, refactored to make API call work and only recall every 60 seconds

---
+++ app/connect/followers/index.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import GetFollowers from "./get-followers";
+
+const followerGoal = ({ current }) => Math.pow(10, (current+'').length)
+
+export default function Followers() {
+  const [goalString, setGoalString] = useState("?/?");
+
+  useEffect(() => {
+    GetFollowers().then((followers) => {
+      setGoalString(`${followers}/${followerGoal({current: followers})}`);
+    });
+  }, []);
+
+  return (
+    <div className="ml-[15px] lg:ml-[20px] mt-[5px]">
+      Follower progress: {goalString}
+    </div>
+  );
+}
