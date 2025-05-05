cc895ff9cbe4e0985dc6e9e36bc852899bd26da0
Michael <mikek222@gmail.com>
2025-05-05 13:43:04 -0400

    refactored and moved code to base level instead of deeply nested

---
+++ app/connect/layout.js
@@ -0,0 +1,17 @@
+export const metadata = { title: 'Connect with David Kando' }
+
+export default function Layot({ children }) {
+ 
+  return (
+    <div
+      className={`w-full flex flex-col justify-center items-center
+      p-5 lg:p-10 text-md lg:text-lg
+      bg-gradient-to-b from-slate-100 to-pink-100`}
+    >
+      <div className="title">Let's hang around</div>
+      <div>
+      {children}
+      </div>
+    </div>
+  );
+}
--- app/connect/page.js
+++ app/connect/page.js
@@ -1,51 +1,43 @@
-import Form from './email-list/form';
-import Followers from './followers';
+import Form from "./email-list/form";
+import Followers from "./followers";
 
 export default function Page() {
- 
   return (
-    <div
-      className={`w-full flex flex-col justify-center items-center
-      p-5 lg:p-10 text-md lg:text-lg
-      bg-gradient-to-b from-slate-100 to-pink-100`}
-    >
-      <div className="title">Let's hang around</div>
+    <>
+      <div>Find my latest projects, upcoming live events, and releases!</div>
+      <br />
       <div>
-        <div>Find my latest projects, upcoming live events, and releases!</div>
-        <br />
-        <div>
-          1. Support me with a follow on{" "}
-          <a
-            className="font-bold link"
-            href="https://open.spotify.com/artist/6ep6Hvwexmaa5IqcPxMxqC?si=t24Vsf6LRsKSIy5lV0wvNw"
-            target="_blank"
-          >
-            Spotify
-          </a>
-          .
-        </div>
-        <Followers />
-        <br />
-        <div>
-          2. Follow me on{" "}
-          <a
-            className="font-bold link"
-            href="https://www.instagram.com/davidkandomusic/"
-            target="_blank"
-          >
-            instagram
-          </a>
-          .
-        </div>
-        <br />
-        <div>3. Join my email list.</div>
-        <Form />
-        <br />
-        <div>
-          Reach out for inquiries at{" "}
-          <span className="underline">davidkandomusic@gmail.com</span>
-        </div>
+        1. Support me with a follow on{" "}
+        <a
+          className="font-bold link"
+          href="https://open.spotify.com/artist/6ep6Hvwexmaa5IqcPxMxqC?si=t24Vsf6LRsKSIy5lV0wvNw"
+          target="_blank"
+        >
+          Spotify
+        </a>
+        .
       </div>
-    </div>
+      <Followers />
+      <br />
+      <div>
+        2. Follow me on{" "}
+        <a
+          className="font-bold link"
+          href="https://www.instagram.com/davidkandomusic/"
+          target="_blank"
+        >
+          instagram
+        </a>
+        .
+      </div>
+      <br />
+      <div>3. Join my email list.</div>
+      <Form />
+      <br />
+      <div>
+        Reach out for inquiries at{" "}
+        <span className="underline">davidkandomusic@gmail.com</span>
+      </div>
+    </>
   );
 }
